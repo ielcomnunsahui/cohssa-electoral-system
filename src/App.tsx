c19f552bfff932cadb78c438c99683369216db79
ielcomnunsahui <ielcomnunsahui@gmail.com>
2026-01-31 11:27:07 +0100

    hhh

--- src/App.tsx
+++ src/App.tsx
@@ -220,15 +220,16 @@ const App = () => (
                 } 
               />
               <Route path="/committee" element={<Committee />} />
-              
-              
+
+              <Route path="/voter/login" element={<VoterLogin />} />
               <Route path="/voter/help" element={<VoterHelpDesk />} />
               <Route 
                 path="/voter/dashboard" 
                 element={
-                  <ProtectedRoute redirectTo="/">
+                  <ProtectedRoute redirectTo="/voter/login">
                     <VoterDashboard />
                   </ProtectedRoute>
+                
                 } 
               />
               <Route path="/aspirant/login" element={<AspirantLogin />} />
